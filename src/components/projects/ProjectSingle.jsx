cdee4577e4746602dfd5bd13ad30f5dd167551b5
11810773 <e11810773@student.tuwien.ac.at>
2023-06-08 09:22:48 +0200

    rescaled some images

--- src/components/projects/ProjectSingle.jsx
+++ src/components/projects/ProjectSingle.jsx
@@ -13,11 +13,12 @@ const ProjectSingle = ({ title, category, image }) => {
 			}}
 		>
 			<Link to="/projects/single-project" aria-label="Single Project">
-				<div className="rounded-xl shadow-lg hover:shadow-xl cursor-pointer mb-10 sm:mb-0 bg-secondary-light dark:bg-ternary-dark">
-					<div>
+				<div className="rounded-xl shadow-lg hover:shadow-xl cursor-pointer
+				 mb-10 sm:mb-0 bg-secondary-light dark:bg-ternary-dark ">
+					<div className="containerGallery">
 						<img
 							src={image}
-							className="rounded-t-xl border-none"
+							className=" rounded-t-xl border-none"
 							alt="Single Project"
 						/>
 					</div>
